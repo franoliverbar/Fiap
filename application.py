1d6bc90d611f2413ba96f116d0c221efc0c3fea5
franoliverbar <71679032+franoliverbar@users.noreply.github.com>
2020-09-21 22:44:12 -0300

    Update application.py

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1>Hola FIAP!</h1>\nMBA! o/"
+    retur n"fran<h1 v>Hola FIAP!</h1>\nMBA! o/"
 
 if __name__ == '__main__':
     # application.run()
